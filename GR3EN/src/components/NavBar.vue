e12d61b138e91078265e007e036bd11763678027
João Pais <jgjoaogabriel@hotmail.com>
2022-12-19 22:59:40 +0000

    Start

---
+++ GR3EN/src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="nav" >
+        <v-app >
+            <v-app-bar :elevation="2">
+                <v-btn variant="text">Logo</v-btn>
+                <v-btn variant="text">Atividades</v-btn>
+                <v-btn variant="text">Ocorrências</v-btn>
+                <v-btn variant="text">Ranking</v-btn>
+                <template v-slot:append>
+                    <v-btn variant="text">Sign In</v-btn>
+                </template>
+            </v-app-bar>
+               
+               
+        </v-app>
+    </div>
+</template>
+
+<script>
+    export default {};
+</script>
+
+<style lang="scss" scoped>
+    .nav{
+        position: absolute;
+        top: 0px;
+        z-index: 2;
+        
+
+    }
+</style>
---
+++ GR3EN/src/views/LandingPageView.vue
@@ -0,0 +1,68 @@
+<script>
+    
+import NavBar from '../components/NavBar.vue'
+export default {
+    components:{
+    NavBar
+    }
+};
+
+</script>
+<template >
+    <div class="page">
+
+        <NavBar />
+            <v-carousel :color="blue">
+                <v-carousel-item
+                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                cover
+                ></v-carousel-item>
+                
+                <v-carousel-item
+                src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+                cover
+                ></v-carousel-item>
+            
+                <v-carousel-item
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                cover
+                ></v-carousel-item>
+            </v-carousel>
+            
+            <section  class="background">
+                <div class="flex">
+                    <img src="https://images.theoutbound.com/uploads/1409240279880/pzozm86m91ux47vi/03460ef5b7c07b97816071b2194d0b42?w=900&fit=crop&dpr=2"
+                     class="homeIMG" alt="">
+                    <h1>loremaskdjnaskjdsand
+
+                    </h1>
+                </div>
+            </section>
+            
+    </div>
+
+        
+
+
+</template>
+
+
+<style lang="scss" scoped>
+    .background{
+        margin-top:5%;
+        background-color:#007362;
+        height:40vh;
+        
+    }
+    .homeIMG{
+        width:20vw;
+        margin:3%
+    }
+    .page{
+        background-image:url(https://images.theoutbound.com/uploads/1409240279880/pzozm86m91ux47vi/03460ef5b7c07b97816071b2194d0b42?w=900&fit=crop&dpr=2);
+        height:300vh;
+    }
+    .flex{
+        display:flex
+    }
+</style>
