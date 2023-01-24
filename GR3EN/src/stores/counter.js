0956d1b5fa04a289f1527c8115ef315d2a9a1648
taisaalho <taisaalho@gmail.com>
2023-01-24 12:33:20 +0000

    Store 1

--- GR3EN/src/stores/counter.js
+++ GR3EN/src/stores/counter.js
@@ -1,62 +1,110 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//USERS
+let users 
+if (!JSON.parse(localStorage.getItem('users'))){
+  users = [{
+    idUser: 0,
+    primeiroNome: " ",
+    ultimoNome: " ",
+    escola: " ",
+    email: " ",
+    password: " ",
+    dataNascimento: " ",
+    idBadge: [],
+    idTitulo: [],
+    questionario: false,
+    biografia: " ",
+    ranking: " ",
+    idOcorrencia: [], 
+    idAtividade: [],
+    conselhoEco: false,
+  }]
+  localStorage.setItem('users', JSON.stringify(users))
+}else{
+  users = JSON.parse(localStorage.getItem('users'))
+}
+
 export const User = defineStore('user', {
   state: () => ({
-    users: [{
-      primeiroNome: "",
-      ultimoNome: "",
-      escola: "",
-      email: "",
-      password: "",
-      badges: "",
-      questionario: "",
-      biografia: "",
-      ranking: "",
-      ocorrênciasRegistadas: "", 
-      atividadesParticipadas: "",
-      conselhoEco: false,
-      atividadesInscritas: "",
-    }]
-  })
+    users: users
+  }),
   
+  getters: {
+    //Get de toda a informação
+    getUser: (state) => state.users, //Não deverá ser usar
+
+    //Escola
+    getEscola: (state) =>
+    (escola) => state.users.find(user => user.escola == escola),  
+
+    //Email
+    getEmail: (state) =>
+    (email) => state.users.find(user => user.email == email),  
+
+    //Password
+    getPassword: (state) =>
+    (password) => state.users.find(user => user.password == password), 
+
+    //Biografia
+    getBiografia: (state) =>
+    (biografia) => state.users.find(user => user.biografia == biografia),
+
+    //Badges
+    getBadges : (state) =>
+    (badges) => state.users.find(user => user.badges == badges),
+
+    //Títulos
+    getTít
+    
+    
+  },
+  actions: {
+    nomeUser(){
+      nome = this.users.primeiroNome + " " + this.users.ultimoNome
+    }
+
+  }
 })
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//ATIVIDADES
+let atividades
+if (!JSON.parse(localStorage.getItem('atividades'))){
+  atividades = [{
+    idAtividade: 0,
+    nomeAtividade: "",
+    idUser: [],
+    descAtividade: " ",
+    imagemAtividade: " ",
+    dataHoraAtividade: " ",
+    localAtividade: [],
+    coordenadorAtividade: [],
+    pontosAtividades: 0,
+    statusAtivdidade: false,
+
+  }]
+  localStorage.setItem('atividades', JSON.stringify(atividades))
+}else{
+  atividades = JSON.parse(localStorage.getItem('atividades'))
+}
 
 export const Atividade = defineStore('atividade',{
   state: () => ({
-    atividades: [{
-      nome: " ",
-      descrição: " ",
-      numInscrições: 0,
-      data: " ",
-      organizador: " ", 
-      user: " ",
-      pontos: " ",
-    }],
-  }),
-})
-
-export const Ocorrência = defineStore('ocorrência',{
-  state: () => ({
-    ocorrências: [{
-      nome: " ",
-      descrição: " ",
-      estado: " ",
-      data: " ",
-      user: " ",
-      foto: " ",
-      pontos: " ",
-    }],
+    atividades: atividades
   }),
 
-
-  getters:{
-    /* ATIVIDADES */
-    getAtividades: (state) => state.atividades,
+  getters: {
+    /* ATIVIDADES */ //Não completo
+    //Get de toda a informação das Atividades
+    getAtividade: (state) => state.atividades,
 
     //Nome da Atividade
     getAtividadesNome: (state) =>
-    (nome) => state.atividades.find(atividade => atividade.nome == nome),
+    (nomeAtividade) => state.atividades.find(atividade => atividade.nomeAtividade == nomeAtividade),
 
     //Descrição da Ativadade
     getAtividadesDescrição: (state) => 
@@ -81,15 +129,46 @@ export const Ocorrência = defineStore('ocorrência',{
     //Pontos que a Atividade dá ao User
     getAtividadesUserUser: (state) =>
     (pontos) => state.atividades.filter(atividade => atividade.pontos == pontos),
+  }
+})
 
-    /* Ocorrências */
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//OCORRÊNCIAS
+let ocorrencias
+if (!JSON.parse(localStorage.getItem('ocorrencias'))){
+  ocorrencias: [{
+    idOcorrencia: 0,
+    nomeOcorrencia: " ",
+    descriçaoOcorrencia: " ",
+    localOcorrencia: " ",
+    estado: " ",
+    dataHoraOcorrencia: " ",
+    idUser: 0,
+    fotoOcorrencia: " ",
+    pontosOcorrencias: 0,
+    categoriaOcorrencia: " ",
+    statusOcorrencia: false,
+  }],
+  localStorage.setItem('ocorrencias', JSON.stringify(ocorrencias))
+}else{
+  ocorrencias = JSON.parse(localStorage.getItem('ocorrencias'))
+}
+
+export const Ocorrencia = defineStore('ocorrencia',{
+  state: () => ({
+    ocorrencias: ocorrencias
+  }),
+
+  getters:{
+    /* Ocorrências */ //Não completo
     //Nome da Ocorrência
     getOcorrênciasNome: (state) =>
-    (nome) => state.ocorrências.filter(ocorrência => ocorrência.nome == nome),
+    (nome) => state.ocorrencias.filter(ocorrencia => ocorrencia.nome == nome),
 
     //Descrição da Ocorrência
     getOcorrênciasDescrição: (state) =>
-    (descrição) => state.ocorrências.filter(ocorrência => ocorrência.descrição == descrição),
+    (descrição) => state.ocorrencias.filter(ocorrencia => ocorrencia.descrição == descrição),
 
     //Estado da Ocorrência
     getOcorrênciasEstado: (state) =>
@@ -102,8 +181,78 @@ export const Ocorrência = defineStore('ocorrência',{
     //User que reportou a Ocorrência
     getOcorrênciasUser: (state) =>
     (user) => state.atividades.filter(atividade => atividade.user == user),
-
-
   },
 })
 
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//BADGES
+let badges
+if (!JSON.parse(localStorage.getItem('badges'))){
+  badges:[{
+    idBadge: 0,
+    descBadge: " ",
+    dataHoraBadge: " ",
+    imagemBadge: " ",
+    idUser: [],
+    pontosBadge: 0,
+  }]
+  localStorage.setItem('badges', JSON.stringify(badges))
+}else{
+  badges = JSON.parse(localStorage.getItem('badges'))
+}
+
+export const Badge = defineStore('badge',{
+  state: () => ({
+    badges: badges
+  }),
+
+  getters: {
+
+  }
+})
+
+
+//------------------------------------------------------------------------------------------------
+//TÍTULOS
+let titulos
+if (!JSON.parse(localStorage.getItem('titulos'))){
+  titulos:[{
+    idTitulo: 0,
+    descTitulo: " ",
+    dataHoraTitulo: " ",
+    imagemTitulo: " ",
+    idUser: [],
+    pontosTitulo: 0,
+  }]
+  localStorage.setItem('titulos', JSON.stringify(titulos))
+}else{
+  titulos = JSON.parse(localStorage.getItem('titulos'))
+}
+
+export const Titulo = defineStore("titulo", {
+  state: () => [{
+    titulos: titulos
+  }],
+})
+
+
+//--------------------------------------------------------------------------------------------------------------------------------
+//RANKING
+let rank
+if (!JSON.parse(localStorage.getItem('rank'))){
+  rank:[{
+    idUser: 0,
+    pontosTotal: 0,
+    ranking: 0,
+    top3Rank: false,
+  }]
+  localStorage.setItem('rank', JSON.stringify(rank))
+}else{
+  rank = JSON.parse(localStorage.getItem('rank'))
+}
+export const Ranking = defineStore("ranking", {
+  state: () => [{
+    rank: rank
+  }],
+})
