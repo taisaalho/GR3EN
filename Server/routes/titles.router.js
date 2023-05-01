c76f17d5ff1175cfe23153c36869675b0650417d
jwted <jgjoaogabriel@hotmail.com>
2023-05-01 22:42:56 +0100

    Occurrences and titles changes

--- Server/routes/titles.router.js
+++ Server/routes/titles.router.js
@@ -1,4 +1,13 @@
 const express = require('express')
+const { createTitle, getTitles, editTitle, deleteTitle } = require('../controllers/titles.controller')
 const router = express.Router()
+const {auth_user,auth_admin} = require('../controllers/auth.controller')
 
+router.route('/')
+    .get(getTitles)
+    .post(auth_admin, createTitle)
+
+    router.route('/:id')
+    .put(auth_admin,editTitle)
+    .delete(auth_admin,deleteTitle)
 module.exports = router
