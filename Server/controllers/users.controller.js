f9302bf8ec235a0bd29e1a8a8c6e23fe0e1689e9
taisaalho <taisaalho@gmail.com>
2023-05-05 14:53:47 +0100

    Teste2

--- Server/controllers/users.controller.js
+++ Server/controllers/users.controller.js
@@ -10,22 +10,22 @@ module.exports={
         .catch(err => res.status(400).json({error: err.message}))
 
     },
-    getUsers:() => {
+    getUsers:(req,res) => {
         let {length=null, offset=null, users = null} = req.query
         if(users)  
             users = users.split(',')
         if(length && offset){
     
-            User.find().skip(offset).limit(10).then(users => { res.status(206).json(users)}).catch(err => { res.status(400).send({err: err.message})})
+            User.find().skip(offset).limit(length).then(users => { res.status(206).json(users)}).catch(err => { res.status(400).send({err: err.message})})
          
         }else if(users){
-            User.find().where('idUser').in(users)
+            User.find().where('_id').in(users)
             .then((users) => { res.status(206).json(users) })
             .catch(err => res.status(500).send({error: err.message}))
         }
     },
 
-    getPartUser: () => {
+    getPartUser: (req,res) => {
         // user = { primeiroNome:String, ultimoNome:String, ultimoNome:String, escola:String, password : String , email : String,  }
         let {length=null, offset=null, users = null} = req.query
         if(users)  
@@ -39,51 +39,56 @@ module.exports={
             .catch(err => res.status(500).send({error: err.message}))
         } 
     },
-    register: async (req,res) => {
 
-/*     newUser:(req,res) => {
-        User.create(req.body)
-        .then((users) => {res.status(200).send(users)})
-        .catch((err) => {res.status(400).send({error: err.message})})
-    }, */
-
-    editUser: () => {
+    editUser: (req,res) => {
         User.findOneAndUpdate({_id: res.params.userid}, req.params)
         .select('primeiroNome ultimoNome escola email password questionario conselhoEco')
         .then((user) => {res.status(201).send(user)})
         .catch((err) =>{res.status(500).send({err:err.message})})
     },
     
-    deleteUser: () =>{
+    deleteUser: (req,res) =>{
         User.findOneAndDelete({_id: res.params.userid}, req.params)
         .then(() => {res.status(204).send({message:`Sucessful deleted`})})
         .catch((err) =>{res.status(500).send({err:err.message})})
     },
 
-    titles : () => {
+    titles : (req,res) => {
         User.find().where('_id')
         .select('idTitulo')
         .then((users) => { res.status(200).json(users) })
         .catch(err => res.status(500).send({error: err.message}))
         },
     
-    badges: () => {
+    badges: (req,res) => {
         User.find().where('_id')
         .select('idBadge')
         .then((users) => { res.status(200).json(users) })
         .catch(err => res.status(500).send({error: err.message}))
-    }
-    ,login:() => {
-        User
-    }
-    ,register:() => {
+    },
+    login: async (req,res) => {
+        console.log(req.body)
+        User.findOne({'email':req.body.email}).then(user => {
+        
+            bcrypt.compare(req.body.password,user.password).then(result => {
+                console.log(result)
+                if(result){
+                    const token = jwtHelpers.createToken(user.id)
+                    res.status(200).cookie('jwt',token).json({Token:token})
+                }else{
+                    res.status(400).json({error: 'Wrong Password'})
+                }
+            })
+           
+        }).catch(err => res.status(400))
+
+    },
+    register:(req,res) => {
         User.create(req.body)
         .then(user => {
             const token = jwtHelpers.createToken(user.id)
             res.status(200).cookie('jwt',token).json({Token:token})
         })
         .catch(err => res.status(400).json({error: err.message}))
-    },LOG:(req,res)=>{
-        console.log(req.body)
     }
 }
--- Server/routes/users.router.js
+++ Server/routes/users.router.js
@@ -2,28 +2,17 @@ const express = require('express')
 const router = express.Router()
 const {getUsers,register,login} =require('../controllers/users.controller')
 const {auth_user,auth_admin} = require('../controllers/auth.controller') // Authentication Middleware 
-const {getUsers,newUser,editUser,deleteUser,titles,badges} = require('../controllers/users.controller.js')
+const {newUser,editUser,deleteUser,titles,badges} = require('../controllers/users.controller.js')
 const User = require('../models/user.model')
 
 
 router.route('/')
     .post(register)
-
-        /* res.status(200).send("Successful request")
-
-        if (false){
-            res.status(401).send(`Missing Fields.The fields are ${}.`)
-        }
-
-        if (false){
-            res.status(500).send("Something went wrong.Please try again later.")
-        } */
-        
+    .get(auth_user,getUsers)
         
 router.route('/login').get(login)
 
 router.route('/:userid')
-    .get(auth_user,getUsers)
     .put(auth_user,editUser)
     .delete(auth_user,deleteUser)
 
