32a43441ff59a9dc7a5c6b58e277693c2e9b2f85
n0th1ng999 <tiagogabrielpereira@gmail.com>
2023-06-16 17:02:50 +0100

    Activity Creation and Edit done

--- GR3EN/src/views/ActivityView.vue
+++ GR3EN/src/views/ActivityView.vue
@@ -3,6 +3,7 @@
         <NavBar/>
         
         <v-sheet class="background">
+            <v-col>
             <v-row>
 
                 
@@ -55,15 +56,55 @@
                     <h1>{{ atividade.descAtividade }}</h1>
                 </v-col>
             </v-row>
-            
-            
-                <v-btn v-if="Coordinator" @click="toggleAtt" class="top btnInscrever">Mudar o estado da ativade</v-btn>
+            <v-row v-if="Coordinator">
 
+                
+                    <v-btn v-if="atividade.statusAtividade" color="red" @click="toggleAtt" class="top btnInscrever">Em Andamento</v-btn>
+                    <v-btn v-if="!atividade.statusAtividade" color="success" @click="toggleAtt" class="top btnInscrever">Encerrar</v-btn>
+             
+            </v-row>
+            <v-row>
+                <v-table >
+                    <thead>
+                        <tr>
+                            <th>
+                                Lista de alunos que não executaram a atividade
+                            </th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="user in atividade.participantesAtividadeNaoExecutado"><td>{{user.primeiroNome + ' ' + user.ultimoNome}} <v-btn v-if="Coordinator"  @click="changeUserState(user,false)" rounded flat>✔️</v-btn></td></tr>
+                    </tbody>
+                </v-table>
+
+                <v-table >
+                    <thead>
+                        <tr>
+                            <th>
+                                Lista de alunos que executaram a atividade
+                            </th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="user in atividade.participantesAtividadeExecutado"><td>{{user.primeiroNome + ' ' + user.ultimoNome}} <v-btn v-if="Coordinator" @click="changeUserState(user,true)" rounded flat>❌</v-btn></td></tr>
+                    </tbody>
+                </v-table>
+                
+                
+                
+            </v-row>
+            <v-row>
+                <v-btn v-if="!enrolledState" @click="Enroll">Enroll</v-btn>
+                <v-btn v-if="enrolledState" @click="unEnroll">UnEnroll</v-btn>
+            </v-row>
+        </v-col>
 
         </v-sheet>
     </v-app>
 </template>
-    
+
 <script>
 
     import {User} from '../stores/userStore.js'
@@ -79,36 +120,12 @@
             userStore: User(),
             atividadesStore: Atividade(),
             atividade:Object,
-            Coordinator:false
-            
+            Coordinator:false,
+            AtividadeState:false,
+            enrolledState:false
+                
         }),
-        computed: {
-            ToggleButtonText(){
-                let atividade = this.atividadesStore.getAtividadeByID(this.atividade.idAtividade)
-    
-                if(atividade.idUser.find(user => user == this.currentUser.idUser)){
-                    return 'Remover Inscrição'
-                }
-                else{
-                    return 'Inscrever'
-                    
-                }
-    
-            },
-            ToggleButtonClass(){
-                let atividade = this.atividadesStore.getAtividadeByID(this.atividade.idAtividade)
-    
-                if(atividade.idUser.find(user => user == this.currentUser.idUser)){
-                    return 'red'
-                }
-                else{
-                    return 'success'
-    
-                }
-    
-            },
-
-        },
+        
         
         async created () {
             try {
@@ -124,16 +141,46 @@
                 let CoordinatorId = this.atividade.coordenadorAtividade
 
                 this.atividade.coordenadorAtividade = res.data[0].primeiroNome + ' ' + res.data[0].ultimoNome
+
+               
+                if(this.atividade.participantesAtividadeNaoExecutado[0]){
+
+                    res = await axios.get('https://elegant-slug-woolens.cyclic.app/users?users=' +  this.atividade.participantesAtividadeNaoExecutado.join())
+                    
+                    for (const i in this.atividade.participantesAtividadeNaoExecutado) {
+                        this.atividade.participantesAtividadeNaoExecutado[i] = res.data[i]
+                    }                
+                
+                }
+                
+                console.log(this.atividade.participantesAtividadeExecutado[0] ? true : false)
+                
+                if(this.atividade.participantesAtividadeExecutado[0]){
+
+                    res = await axios.get('https://elegant-slug-woolens.cyclic.app/users?users=' +  this.atividade.participantesAtividadeExecutado.join())
+    
+                    for (const i in this.atividade.participantesAtividadeExecutado) {
+                        this.atividade.participantesAtividadeExecutado[i] = res.data[i]
+                    }                
+
+                }
+
                 
                 res = await axios.get('https://elegant-slug-woolens.cyclic.app/users/user-profile',{
                     headers:{
                         Authorization:'Bearer '+ localStorage.getItem('Token')
                     }
                 })
-               
+                
+                if(this.atividade.participantesAtividadeExecutado.some(user => user._id == res.data._id) || this.atividade.participantesAtividadeNaoExecutado.some(user => user._id == res.data._id)){
+                    this.enrolledState = true
+                }
+
                 if(CoordinatorId == res.data._id){
                     this.Coordinator = true
                 }
+                
+                
 
             } catch (error) {
                 
@@ -144,12 +191,78 @@
         methods: {
 
             async toggleAtt(){
+
+                console.log(this.$route.params.id)
+
                 try {
-                    let res = await axios.patch('https://elegant-slug-woolens.cyclic.app/'+ this.$route.params.id +'/change-activity-state')
+
+                    let res = await axios.patch('https://elegant-slug-woolens.cyclic.app/activities/'+ this.$route.params.id +'/change-activity-state',null,{
+                    headers:{
+                        Authorization:'Bearer ' + localStorage.getItem('Token')
+                    }
+                    })
+
+
+                    this.atividade.statusAtividade = !this.atividade.statusAtividade 
+
+
                 } catch (error) {
                     
                 }
             },
+            async changeUserState(user,change){
+                try {
+                    
+                    const activityid = this.$route.params.id
+                    
+                    const userid = user._id
+                    
+                    let res = await axios.patch(`https://elegant-slug-woolens.cyclic.app/activities/${activityid}/users/${userid}/change-user-state`,null,{headers:{Authorization:'Bearer '+localStorage.getItem('Token')}})
+
+                    if(change){
+                        this.atividade.participantesAtividadeExecutado = this.atividade.participantesAtividadeExecutado.filter(u => u._id != userid)
+                        
+                        this.atividade.participantesAtividadeNaoExecutado.push(user)
+                        
+                    }else{
+                        this.atividade.participantesAtividadeNaoExecutado = this.atividade.participantesAtividadeNaoExecutado.filter(u => u._id != userid)
+                        
+                        this.atividade.participantesAtividadeExecutado.push(user)
+                        
+                    }
+                } catch (error) {
+                    
+                }
+                
+                
+            },
+            async Enroll(){
+                try {
+
+                    const activityid = this.$route.params.id
+                    
+                    let res = await axios.post(`https://elegant-slug-woolens.cyclic.app/activities/${activityid}/enroll`,null,{headers:{Authorization:'Bearer '+localStorage.getItem('Token')}})
+                    
+                    location.reload()
+
+                } catch (error) {
+                    
+                }
+            },
+            async unEnroll(){
+                
+                try {
+                    const activityid = this.$route.params.id
+                    
+                    let res = await axios.delete(`https://elegant-slug-woolens.cyclic.app/activities/${activityid}/enroll`,{headers:{Authorization:'Bearer '+localStorage.getItem('Token')}})
+                    
+                    location.reload()
+
+                } catch (error) {
+                    
+                }
+            }
+            
             
           
         
